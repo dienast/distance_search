ab09d582bf655a6a0d6ae10ca01e8031f071bc65
dienast <byronschuurman@live.nl>
2013-11-26 14:06:35 +0100

    Create update.sql

---
+++ update.sql
@@ -0,0 +1,2 @@
+ALTER TABLE /*TABLE_PREFIX*/t_city ADD COLUMN d_coord_lat DECIMAL(10,6) NULL AFTER b_active;
+ALTER TABLE /*TABLE_PREFIX*/t_city ADD COLUMN d_coord_long DECIMAL(10,6) NULL AFTER b_active;
